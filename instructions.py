a56e77939ce17e2f1c590e2155888387d6de81ac
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2024-09-21 03:19:14 -0300

    Update instructions.py
    Adicionei o método de impressão de dados alternativo

--- instructions.py
+++ instructions.py
@@ -1,113 +1,157 @@
-'''
-    A ideia é mapear os padrões da arquitetura Jouette para as instruções de máquina,
-    assim, a partir de um padrão de um nó da árvore de expressão, é possível gerar
-    a instrução de máquina correspondente.
-'''
+from typing import *
+from node import Node
 
-from tree import *
+LEFT_CHILD = False
+RIGHT_CHILD = True
 
-PATTERNS_INSTRUCTIONS = {
-    "TEMP": "TEMP r{i}", # TEMP
-    "FP": "FP r{i}", # FP
-    "+": "ADD r{i} <- {j} + {k}", # +
-    "*": "MUL r{i} <- {j} * {k}", # *
-    "-": "SUB r{i} <- {j} - {k}", # -
-    "/": "DIV r{i} <- {j} / {k}", # /
-    "+ -> CONST": "ADDI r{i} <- {j} + {c}", # + -> CONST
-    "+ --> CONST": "ADDI r{i} <- {j} + {c}", # + --> CONST
-    "CONST": "ADDI r{i} <- r{j} + {c}", # CONST
-    "- --> CONST": "SUBI r{i} <- {j} - {c}", # - --> CONST
-    "MEM -> + --> CONST": "LOAD r{i} <- M[{j} + {c}]", # MEM -> + --> CONST
-    "MEM -> + -> CONST": "LOAD r{i} <- M[{j} + {c}]", # MEM -> + -> CONST
-    "MEM -> CONST": "LOAD r{i} <- M[r{j} + {c}]", # MEM -> CONST
-    "MEM": "LOAD r{i} <- M[r{j}]", # MEM
-    "MOVE -> MEM -> + --> CONST": "STORE M[{j} + {c}] <- r{i}", # MOVE -> MEM -> + --> CONST
-    "MOVE -> MEM -> + -> CONST": "STORE M[{j} + {c}] <- r{i}", # MOVE -> MEM -> + -> CONST
-    "MOVE -> MEM -> CONST": "STORE M[r{j} + {c}] <- r{i}", # MOVE -> MEM -> CONST
-    "MOVE -> MEM": "STORE M[r{j}] <- r{i}", # MOVE -> MEM
-    "MOVE -> MEM ===> MEM": "MOVEM M[r{j}] <- M[r{i}]", # MOVE -> MEM ===> MEM
-}
+global whereToinsert
 
-'''
-    A função getInstructions recebe uma lista de listas, onde cada lista contém um nó da árvore de expressão
-    e o padrão correspondente a esse nó. O padrão serve para que a função possa mapear o padrão para a instrução,
-    enquanto o nó serve para que a função possa acessar os valores dos nós da árvore de expressão.
-
-    A lista respeita o seguinte formato:
-
-    [
-        [Node, pattern],
-        [Node, pattern],
-        ...
-    ]
-'''
-
-def getInstructions(patterns):
-    reg1 = 1
-    reg2 = 2
+class Tree():
+    def __init__(self, root = None):
+        self.root = root
+        self.scope = []
+        self.reducibleInstructions = ["MOVE", "MEM", "+", "-", "*", "/"]
+        self.irreducibleInstructions = ["CONST", "FP", "TEMP"]
+        self.basicOperation = ["+", "-", "*", "/"]
+        self.instructions = self.reducibleInstructions + self.irreducibleInstructions
+        self.validSeparators = [",", "(", ")"]
     
-    line_count = 0
+    def isIrredutiableInstruction(self, instruction) -> bool:
+        """
+        Uma instrução é irredutível se não pode ser
+        derivada em outras instruções. Neste caso,
+        elas são as folhas da árvore de instrução.
+        No contexto da arquitetura Joutte, elas per-
+        tecem a este conjunto {CONST X, FP, TEMP i}
+        """        
+        return instruction in self.irreducibleInstructions
+   
+    def isCompoundInstruction(self, instruction:str) -> bool:
+        """
+        Uma instrução é dita composta se existe um espaço
+        entre o seu tipo e o valor associado a ele. Por
+        exemplo, 'CONST 2' é uma instrução composta, pois
+        existe um espaço separando o 'CONST' e o '2'.
+        Todas as instruções compostas são irredutíveis,
+        mas o inverso não é verdadeiro.
+        """
+        return instruction in ["CONST", "TEMP"]
 
-    patterns = patterns[::-1]
+    def isBasicOperation(self, instruction):
+        return instruction in self.basicOperation
 
-    for i in range(len(patterns)):
-        node = patterns[i][0]
-        pattern = patterns[i][1]
+    def printTree(self, node: Node, indent: str = "", isRight: bool = False):
+        if node is not None:
+            prefix = "└── " if isRight else "┌── "
+            print(indent + prefix + node.instruction)
+            new_indent = indent + ("    " if isRight else "│   ")
+
+            if node.right is not None:
+                self.printTree(node.right, new_indent, True)
+            if node.left is not None:
+                self.printTree(node.left, new_indent, False)
+    
+    def draw(self):
+        self.__draw(self.root)
+    
+    def __draw(self,node:Node, amountSpaces=0):
+        if (node != None):
+            currentAmountSpaces = amountSpaces+4
+            if (node == self.root):
+                print((" "*amountSpaces)+node.instruction)
+                self.__draw(node.right, currentAmountSpaces)
+                self.__draw(node.left, currentAmountSpaces)
+            elif (node.parent.instruction == "MEM" and ((self.isIrredutiableInstruction((((node.instruction).split())[0]))) or self.isBasicOperation(node.instruction)) and node.parent.right == None):
+                print((" "*amountSpaces)+"("+node.instruction+")")
+                self.__draw(node.right, currentAmountSpaces)
+                self.__draw(node.left, currentAmountSpaces)
+            else:
+                print((" "*amountSpaces)+node.instruction)
+                if (node.left != None and node.right != None):
+                    self.__draw(node.right, currentAmountSpaces)
+                    self.__draw(node.left, currentAmountSpaces)
+                else:
+                    self.__draw(node.left, currentAmountSpaces)
+                    self.__draw(node.right, currentAmountSpaces)
+
+
+    
+    def createTree(self, linearCode:str):
+        spacelessLinearCode = linearCode.split()
+        reducibleInstructions = []
         
-        instruction = PATTERNS_INSTRUCTIONS[pattern]
+        #for instruction in spacelessLinearCode:
+        #    if instruction in self.reducibleInstructions:
+        #        reducibleInstructions.append(instruction)
+        """
+        De início, a insertção dos filhos começa sempre
+        nas sub-árvores esquerdas. Só começamos a inserir
+        nas subárvores direitas quando lemos uma vírgula. 
+        """
+        whereToInsert = LEFT_CHILD #ORIGINAL
+        #global whereToinsert
+        currentNode = None
+        previousNode = None
+        
+        indexInstruction = 0
 
-        line_count += 1
+        while (indexInstruction < len(spacelessLinearCode)):
+            #nonlocal whereToinsert
+            currentInstruction = spacelessLinearCode[indexInstruction]
             
-        if(pattern in ["+", "*", "-", "/"]):
-            print(line_count, instruction.format(i=reg2, j=node.left.getSingleValue(reg1), k=node.right.getSingleValue(reg2)))
-
-        if(pattern in ["+ -> CONST", "+ --> CONST", "- --> CONST"]):
-            right = node.right.getSingleValue(reg1) 
-            left = node.left.getSingleValue(reg1+1)
-
-            if(pattern == "+ -> CONST" or pattern == "- --> CONST"):
-                print(line_count, instruction.format(i=reg2, j=right, c=left))
-
-            else:
-                print(line_count, instruction.format(i=reg2, j=left, c=right))   
-            
-        if(pattern == "CONST"):
-            print(line_count, instruction.format(i=reg2, j=0, c=node.getSingleValue(reg1)))
-            
-        if (pattern in ["MEM -> + --> CONST", "MEM -> + -> CONST"]):
-            child = node.getChildren()[0]
-            right = child.right.getSingleValue(reg1) 
-            left = child.left.getSingleValue(reg1+1)
-
-            if (pattern == "MEM -> + --> CONST"):
-                print(line_count, instruction.format(i=reg2, j=left, c=right))
-            else:
-                print(line_count, instruction.format(i=reg2, j=right, c=left))  
-            
-        if(pattern == "MEM -> CONST"):
-            child = node.getChildren()[0]
-
-            print(line_count, instruction.format(i=reg2, j=0, c=child.getSingleValue(reg1)))
-
-        if (pattern == "MEM"):
-            print(line_count, instruction.format(i=reg2, j=reg2, c=0))
-
-        if(pattern in ["MOVE -> MEM -> + --> CONST", "MOVE -> MEM -> + -> CONST"]):
-            child = node.getChildren()[0].getChildren()[0]
-            right = child.right.getSingleValue(reg1) 
-            left = child.left.getSingleValue(reg1+1)
-
-            if(pattern == "MOVE -> MEM -> + --> CONST"):
-                print(line_count, instruction.format(i=reg2, j=left, c=right))
-            else:
-                print(line_count, instruction.format(i=reg2, j=right, c=left))  
-
-        if(pattern == "MOVE -> MEM -> CONST"):
-            child = node.getChildren()[0].getChildren()[0]
-            print(line_count, instruction.format(i=reg2, j=0, c=child.getSingleValue(reg1)))
-
-        if(pattern == "MOVE -> MEM"):
-            print(line_count, instruction.format(i=reg2, j=reg1, c=0))
-            
-        if(pattern == "MOVE -> MEM ===> MEM"):
-            print(line_count, instruction.format(i=reg2, j=reg1))
+            """
+            Se lermos uma instrução composta, é necessário avançar o index 2 vezes,
+            pois se a primeira metade da instrução composta estiver no i-ésimo
+            indice, a sua segunda metade estará no i+1-ésimo indice, o que fará
+            com que tenhamos que pular um indice ao final do código. A flag 
+            "jumpIndex" indica a necessidade de pular mais um indice.
+            """
+            jumpIndex = False
+            if (currentInstruction in self.instructions):
+                if (self.isCompoundInstruction(currentInstruction)):
+                    """
+                    É necessário concatenar as duas partes
+                    """
+                    secondHalfInstruction = spacelessLinearCode[indexInstruction+1]
+                    currentInstruction += " " + secondHalfInstruction
+                    jumpIndex = True
+                
+                newNode = Node(currentInstruction)
+                
+                """
+                Se eu não tiver nenhum escopo pronto,
+                significa que a árvore está vazia e 
+                que é necessário criá-la
+                """
+                if (len(self.scope) == 0):
+                    self.root = newNode
+                    previousNode = newNode
+                
+                elif (previousNode):
+                    newNode.parent = previousNode
+                    if (whereToInsert == LEFT_CHILD):
+                        self.scope[-1].left = newNode
+                    else:
+                        self.scope[-1].right = newNode
+                        whereToInsert = LEFT_CHILD
+                    if (newNode.instruction != "FP" and newNode.instruction[:5] != "CONST" and newNode.instruction[:4] != "TEMP"):
+                        previousNode = newNode
+                
+                if currentInstruction in self.reducibleInstructions:
+                    self.scope.append(newNode)
+                
+                if (jumpIndex):
+                    indexInstruction +=2
+                    continue
+                    
+            elif (currentInstruction == "("):
+                indexInstruction += 1
+                continue
+            elif (currentInstruction == ")"):
+                self.scope.pop()
+                if (len(self.scope) != 0):
+                    previousNode = self.scope[-1]
+            elif (currentInstruction == ","):
+                whereToInsert = RIGHT_CHILD #COPY
+                
+            indexInstruction += 1
